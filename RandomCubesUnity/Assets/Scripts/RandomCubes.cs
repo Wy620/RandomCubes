c27b03b529441e2986f0ce2614106383581d4216
Yao Wang <oscarwangy@gmail.com>
2022-01-24 15:10:26 -0600

    created: cubes
    created: cubes

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,31 @@
+/**
+ * Created by: Yao Wang
+ * Date Created: 1/24/2022
+ * 
+ *Last Edited by: NA
+ *Last Edited: 1/24/2022
+ *
+ *Descreption: RandomCubs
+ * */
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
